50dea13680aa8d05335e317ff32311e6ddda926d
spacegeek224 <marisusisme@gmail.com>
2015-07-22 11:11:47 -0400

    Fixing fibonacci loop

--- FibonacciFinder/Source.cpp
+++ FibonacciFinder/Source.cpp
@@ -2,21 +2,36 @@
 
 using namespace std;
 
-int fibonacci(int n);
+//int fibonacci(int n);
 
 int main() {
 
-	fibonacci(4);
+	//cout << fibonacci(4) << endl;
+
+	int a = 0;
+	int b = 1;
+	int c;
+	for (int i=0; i<3; i++) {
+		c = a + b;
+		cout << a << " + " << b << " = " << c << endl;
+		b = a;
+		a = c;
+		cout << "a = " << a << endl << "b = " << b << endl;
+	}
+	
 
 	return 0;
 
 }
 
-int fibonacci(int n) {
+/*int fibonacci(int n) {
 	int a = 0;
 	int b = 1;
-	for (i=0; i<n; i++) {
-		cout << a << " + " << b << " = " << a + b << endl;
+	int num;
+	for (int i=0; i<n; i++) {
+		//cout << a << " + " << b << " = ";
 		a = a + b;
+		//cout << a << endl;
 	}
-}
+	return a;
+}*/
