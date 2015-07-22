d8debb740aa820871fe4507672777f56308cc47e
spacegeek224 <marisusisme@gmail.com>
2015-07-22 11:20:40 -0400

    Alpha 0.0.1 release

--- FibonacciFinder/Source.cpp
+++ FibonacciFinder/Source.cpp
@@ -5,18 +5,20 @@ using namespace std;
 //int fibonacci(int n);
 
 int main() {
-
-	//cout << fibonacci(4) << endl;
-
-	int a = 0;
-	int b = 1;
-	int c;
-	for (int i=0; i<3; i++) {
-		c = a + b;
-		cout << a << " + " << b << " = " << c << endl;
-		b = a;
-		a = c;
-		cout << "a = " << a << endl << "b = " << b << endl;
+	int usr;
+	string apple;
+	bool cont = true;
+	while (cont)
+	{
+		cout << "Enter the number result in the fibonacci sequence to find, or type \'0\' to quit: ";
+		cin >> usr;
+		cout << endl;
+		if (usr == 0) {
+			cont = false;
+			break;
+		} else {
+			cout << fibonacci(usr) << endl; 
+		}
 	}
 	
 
@@ -24,14 +26,19 @@ int main() {
 
 }
 
-/*int fibonacci(int n) {
+int fibonacci(int n) {
 	int a = 0;
 	int b = 1;
 	int num;
+	int a = 0;
+	int b = 1;
+	int c;
 	for (int i=0; i<n; i++) {
-		//cout << a << " + " << b << " = ";
-		a = a + b;
-		//cout << a << endl;
+		c = a + b;
+		//cout << a << " + " << b << " = " << c << endl;
+		b = a;
+		a = c;
+		//cout << "a = " << a << endl << "b = " << b << endl;
 	}
-	return a;
-}*/
+	return c;
+}
